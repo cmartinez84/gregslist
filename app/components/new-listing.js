bd4ef7e34619e090a312a87b47262fc9ab7e6416
chris martinez and mark lawson <mark@>
2016-10-31 09:56:48 -0700

    clear data in fields on submit, form validation

--- app/components/new-listing.js
+++ app/components/new-listing.js
@@ -2,16 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    post(category){
+    post(){
       var params = {
         title: this.get('title'),
         image: this.get('image'),
         content: this.get('content'),
         author: this.get('author'),
         date: moment().format("LLLL"),
-        category: category
+        category: this.get('category')
       };
-      this.sendAction("post", params);
+      if(params['title'] && params['content'] && params['author']){
+        this.sendAction("post", params);
+        this.set("title", "");
+        this.set("image", "");
+        this.set("content",  "");
+        this.set("author", "");
+      }
     }
   }
 });
