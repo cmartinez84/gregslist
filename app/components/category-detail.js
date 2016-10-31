f17b721f2e462734b0b767353478027247e6f9bb
chris martinez and mark lawson <chris@>
2016-10-31 11:58:26 -0700

    add pagination for listings

--- app/components/category-detail.js
+++ app/components/category-detail.js
@@ -3,7 +3,21 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   sortBy: ['date:desc'],
+  page: 3,
   sortedListings: Ember.computed.sort('category.listings', "sortBy"),
+  pageListings: Ember.computed("sortedListings",function(){
+    var output = [];
+    var offset = (this.get('page')-1) * 5;
+    var i = 0;
+    while (i < 5) {
+      if (this.get('sortedListings')[offset]){
+        output.push(this.get('sortedListings')[offset]);
+      }
+      i++;
+      offset++;
+    }
+    return output;
+  }),
   actions: {
     sortListingChange(){
     let selectedIndex = this.$('#sortListings').val();
